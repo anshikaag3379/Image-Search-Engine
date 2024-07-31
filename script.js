4fdc14f6baf88330ac1484a29f8befd01662a419
Anshika <23ucc516@lnmiit.ac.in>
2024-07-31 11:14:45 +0530

    added api

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 const searchForm=document.getElementById("search-form");
 const searchBox=document.getElementById("search-box");
 const searchResults=document.getElementById("search-result");
-const showMore=documnet.getElementById("show-more-btn")
+const showMore=document.getElementById("show-more-btn")
 
 let keyword="";
 let page=1;
 
 async function searchImages(){
     keyword=searchBox.value;
-    const url= `https://api.unsplash.com/search/photos?query=${keyword}&page=${page}&client_id=yDDA7cvE3R07LwvSmSTiVmOigWVJs4Lj4GnFVnatNKg`;
-    const response=await fetch(url);
-    const data= await response.json();
-    const results=data.results;
-    results.map((result)=>{
-        const img=document.createElement("img");
-        img.src=result.urls.small;
-        searchResults.appendChild(img);
-    }
-}
+    const url= `https://api.unsplash.com/search/photos?query=${keyword}&page=${page}&client_id=yDDA7cvE3R07LwvSmSTiVmOigWVJs4Lj4GnFVnatNKg&per_page=12`;
 
-searchForm.addEventListener("submit"), (e)=>{
+    const response= await fetch(url);
+    const data= await response.json();
+
+    const results= data.results;
+
+    results.map((result)=>{
+        const image= document.createElement("img");
+        image.src=result.urls.small;
+        const imageLink= document.createElement("a");
+        imageLink.href=result.links.html;
+        imageLink.target="_blank";
+
+        imageLink.appendChild(image);
+        searchResults.appendChild(imageLink);
+    })
+    }
+
+
+searchForm.addEventListener("submit", (e)=>{
     e.preventDefault();
     page=1;
     searchImages();
 
-}
+})
